5f0337b4942ae0b853e097245233711edcd21d06
yankees3490 <yankees3490@gmail.com>
2019-06-20 14:40:38 -0700

    Chapter 8- Adding and Removing follows

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -28,6 +33,12 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    followed = db.relationship(
+    'User', secondary=followers,
+    primaryjoin=(followers.c.follower_id == id),
+    secondaryjoin=(followers.c.followed_id == id),
+    backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
 @login.user_loader
 def user_loader(id):
