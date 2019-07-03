d1499371ac803289bdf193a5801a474ee3c33d95
yankees3490 <yankees3490@gmail.com>
2019-07-03 11:17:30 -0400

    Upto Making It Easier to Find Users to Follow

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,8 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from datetime import datetime
+from app.form import PostForm
+from app.models import Post
 
 @app.before_request
 def before_request():
@@ -12,10 +14,17 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
         posts = [
         {
             'author': {'username': 'John'},
@@ -26,8 +35,9 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-        
-        return render_template('index.html', title='Home', posts=posts)
+
+        posts = current_user.followed_posts().all()
+        return render_template('index.html', title='Home Page',form=form,  posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
